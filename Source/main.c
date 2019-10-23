395eb4930771458806c72fc9545b48301c0f8257
unknown <olehnovosad2013@gmail.com>
2019-10-23 21:13:37 +0300

    Comments added.

--- Source/main.c
+++ Source/main.c
@@ -43,15 +43,15 @@ const cy_stc_sysint_t switch_intr_config = {
 const cy_stc_sysint_t sdHostIntrConfig =
 {
 	#if (CY_CPU_CORTEX_M0P)
-		  .intrSrc = NvicMux4_IRQn,
-		  .cm0pSrc = SD_Host_INTR_NUM,
+		  .intrSrc = NvicMux4_IRQn,				/* Source of interrupt signal */
+		  .cm0pSrc = SD_Host_INTR_NUM,			/* Interrupt priority */
 	#else
-		 .intrSrc = SD_Host_INTR_NUM, /* SD Host interrupt number (non M0 core)*/
+		 .intrSrc = SD_Host_INTR_NUM, 			/* SD Host interrupt number (non M0 core)*/
 	#endif
-		 .intrPriority = SD_Host_INTR_PRIORITY
+		 .intrPriority = SD_Host_INTR_PRIORITY	/* SD Host interrupt priority (non M0 core)*/
 };
 
-//SDHC config
+//SDHC configuration
 const cy_stc_sd_host_init_config_t sdHostConfig =
 {
     .dmaType = CY_SD_HOST_DMA_ADMA2,
@@ -59,7 +59,7 @@ const cy_stc_sd_host_init_config_t sdHostConfig =
     .emmc = false,
 };
 
-//SD card config
+//SD card configuration
 cy_stc_sd_host_sd_card_config_t sdCardConfig =
 {
     .lowVoltageSignaling = false,
@@ -77,8 +77,16 @@ __WEAK bool Cy_SD_Host_IsCardConnected(SDHC_Type const *base)
 	return true;
 }
 
-//Function for SW interruption
-void Isr_switch(void)
+/*******************************************************************************
+* Function Name: Isr_switch
+********************************************************************************
+*
+* Summary:
+*  This function is used for SW interruption handling.
+*
+*******************************************************************************/
+
+void Isr_switch()
 {
 	if(temperature_sw == true){
 		temperature_sw = false;
@@ -92,8 +100,16 @@ void Isr_switch(void)
 	NVIC_ClearPendingIRQ(switch_intr_config.intrSrc);
 }
 
-//Function for SDHC interruption
-void SD_Host_User_Isr(void)
+/*******************************************************************************
+* Function Name: SD_Host_User_Isr
+********************************************************************************
+*
+* Summary:
+*  This function is used for SDHC interruption handling.
+*
+*******************************************************************************/
+
+void SD_Host_User_Isr()
 {
     uint32_t normalStatus;
     uint32_t errorStatus;
@@ -117,11 +133,11 @@ void SD_Host_User_Isr(void)
     /* Add the use code here. */
 }
 
-void PrintDateAndTime();
+void PrintDateAndTime(int frequency);
 
 char * ThermistorInfo(char * terminfo);
 
-bool SDHCRW(char * dateandtime);
+bool SDHC_Read_Write(char * dateandtime);
 
 int main(void)
 {
@@ -130,24 +146,29 @@ int main(void)
 
     __enable_irq();
 
-    //Set UART
+    // Variable which use for changing frequency of recording.
+    char ch;
+    // Variable recording frequency
+    int frequency = 30;
+
+    //Set up UART
     Cy_SCB_UART_Init(Bridge_UART_HW, &Bridge_UART_config, &uart_context);
 	Cy_SCB_UART_Enable(Bridge_UART_HW);
 
-	//Set RTC
+	//Set up RTC
 	Cy_RTC_Init(&RTC_config);
 
-	//Set System Analog Reference Block
+	//Set yp System Analog Reference Block
 	cy_en_sysanalog_status_t status_aref;
 	status_aref = Cy_SysAnalog_Init(&Cy_SysAnalog_Fast_Local);
 	Cy_SysAnalog_Enable();
 
-	//Set SAR ADC
+	//Set up SAR ADC
 	Cy_SAR_Init(SAR_HW, &SAR_config);
 	Cy_SAR_Enable(SAR_HW);
 	Cy_SAR_StartConvert(SAR_HW, CY_SAR_START_CONVERT_CONTINUOUS);
 
-	//Set button interrupt
+	//Set up button interrupt
 	Cy_SysInt_Init(&switch_intr_config, Isr_switch);
 	NVIC_ClearPendingIRQ(switch_intr_config.intrSrc);
 	NVIC_EnableIRQ(switch_intr_config.intrSrc);
@@ -157,12 +178,41 @@ int main(void)
 
     for(;;)
     {
-    	PrintDateAndTime();
+    	//Getting value via UART.
+    	ch = Cy_SCB_UART_Get(Bridge_UART_HW);
+
+    	if(ch == 'c'){
+    		printf("With which frequency you want to record the info:\r\n");
+    		fscanf(stdin, "%d", &frequency);
+    	} else {
+    		//Checking of admissibility of "frequency" value.
+    		if(frequency < 1 || frequency > 59){
+				printf("Please choose between <1-59>\r\n");
+				fscanf(stdin, "%d", &frequency);
+			} else{
+				PrintDateAndTime(frequency);
+			}
+		}
     }
 }
 
-void PrintDateAndTime(){
+/*******************************************************************************
+* Function Name: PrintDateAndTime
+********************************************************************************
+*
+* Summary:
+*  This function is getting value of date and time and print out it.
+*
+* Parameters:
+*  frequency: Frequency of recording information to SD card.
+*
+*******************************************************************************/
+
+void PrintDateAndTime(int frequency){
+	//Turn off the LED.
 	Cy_GPIO_Write(Pin_Led_PORT,Pin_Led_NUM,1);
+
+	//Arrays for the information.
 	char dateandtime[CY_SD_HOST_BLOCK_SIZE];
 	char terminfo[CY_SD_HOST_BLOCK_SIZE];
 
@@ -174,21 +224,25 @@ void PrintDateAndTime(){
 
 	if(dateTime.sec != secPrev)
 	{
+		// Remembering the previous second.
 		secPrev = dateTime.sec;
+		// Print out the date and time.
 		printf("<%2u-%2u-%2u %2u:%2u:%2u> ",	\
 				(uint16_t)dateTime.date, (uint16_t)dateTime.month, (uint16_t)dateTime.year,\
 					(uint16_t)dateTime.hour, (uint16_t)dateTime.min, (uint16_t)dateTime.sec);
-
+		// Recording the date and time into char array.
 		sprintf(dateandtime, "<%2u-%2u-%2u %2u:%2u:%2u> ", (uint16_t)dateTime.date, (uint16_t)dateTime.month, (uint16_t)dateTime.year,\
 				(uint16_t)dateTime.hour, (uint16_t)dateTime.min, (uint16_t)dateTime.sec);
 
-		/*Print Thermistor info.*/
+		/*Print out Thermistor info.*/
 		ThermistorInfo(terminfo);
 
+		//Recording the information from thermistor into char array.
 		strncat(dateandtime, terminfo, 20);
 
-		if(secPrev == 0 || secPrev == 30){
-			if(SDHCRW(dateandtime)){
+		// If modulo division is equal to zero, then information is burning to sd card.
+		if(secPrev % frequency == 0){
+			if(SDHC_Read_Write(dateandtime)){
 				Cy_GPIO_Write(Pin_Led_PORT,Pin_Led_NUM,0);
 				Cy_SysLib_Delay(500);
 			}
@@ -196,28 +250,75 @@ void PrintDateAndTime(){
 	}
 }
 
+/*******************************************************************************
+* Function Name: ThermistorInfo
+********************************************************************************
+*
+* Summary:
+*  This function is getting values of voltages from ACD and count temperature
+*  via thermistor and print out it.
+*
+* Parameters:
+*  terminfo: char array for recording information.
+*
+* Return:
+*  Char array with temperature information.
+*
+*******************************************************************************/
+
 char * ThermistorInfo(char * terminfo){
+	//Variables for voltages.
+	float v1,v2;
+
 	int16_t countThermistor, countReference;
 
+	//Getting values from channels.
 	countReference = Cy_SAR_GetResult16(SAR, 0);
 	countThermistor = Cy_SAR_GetResult16(SAR, 1);
 
+	//Getting values of voltages.
+	v1 = Cy_SAR_CountsTo_Volts(SAR_HW, 0, countReference);
+	v2 = Cy_SAR_CountsTo_Volts(SAR_HW, 1, countThermistor);
+
+	//Getting the resistance across the thermistor.
 	uint32 resT = Thermistor_GetResistance(countReference, countThermistor);
+
+	//Counting temperature in Celsius.
 	float temperature = (float)Thermistor_GetTemperature(resT) / 100.0;
 
-	printf("<T = %.3f%c>\r\n", temperature_sw == true ? (temperature) : FAHRENHEIT(temperature),
+	//Print out the voltages and temperature.
+	printf("<v1 = %.3f v2 = %.3f T = %.3f%c>\r\n", v1, v2, temperature_sw == true ? (temperature) : FAHRENHEIT(temperature),
 			temperature_sw == true ? 'C' : 'F');
 
+	// Recording the temperature into char array.
 	sprintf(terminfo, "<T = %.3f%c>\r\n", temperature_sw == true ? (temperature) : FAHRENHEIT(temperature),
 			temperature_sw == true ? 'C' : 'F');
 
 	return terminfo;
 }
 
-bool SDHCRW(char * dateandtime){
+/*******************************************************************************
+* Function Name: SDHC_Read_Write
+********************************************************************************
+*
+* Summary:
+*  This function is used for initialization of SD Host. Also this function is
+*  written information into SD card and read information from there.
+*
+* Parameters:
+*  dateandtime: char array which contains the date, time and temperature information.
+*
+* Return:
+*  true if reading was success and false if recording or reading was unsuccessful.
+*
+*******************************************************************************/
+
+bool SDHC_Read_Write(char * dateandtime){
+	//Initialization of SDHC interruption
 	Cy_SysInt_Init(&sdHostIntrConfig, &SD_Host_User_Isr);
 	NVIC_EnableIRQ(SD_Host_INTR_NUM);
 
+	//Enable SDHC
 	Cy_SD_Host_Enable(SDHC_HW);
 	ret = Cy_SD_Host_Init(SDHC_HW, &sdHostConfig, &sdHostContext);
 
@@ -244,6 +345,7 @@ bool SDHCRW(char * dateandtime){
 		}
 		Cy_SD_Host_ClearNormalInterruptStatus(SDHC1, CY_SD_HOST_XFER_COMPLETE);
 	} else {
+		fprintf(stderr, "Cannot write information.\r\n");
 		return false;
 	}
 
@@ -259,6 +361,7 @@ bool SDHCRW(char * dateandtime){
 		/* Clear the data-transaction complete event. */
 		Cy_SD_Host_ClearNormalInterruptStatus(SDHC1, CY_SD_HOST_XFER_COMPLETE);
 	} else {
+		fprintf(stderr, "Cannot read information.\r\n");
 		return false;
 	}
 
